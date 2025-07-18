79f465cadfb13c0ede828b49a6f4db4dd4b6815f
AndresCelis <celisp27@outlook.com>
2025-07-18 12:40:04 -0500

    [Feature] Adding objects to the HomeView

--- SwifulControlVersion/HomeView.swift
+++ SwifulControlVersion/HomeView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var title: String = "SwifulControlVersion"
+    
     var body: some View {
         Text("Screen2")
+        
+        Text("\(title)")
+        Rectangle()
+            .fill(Color.black)
+            .frame(height: 200)
     }
 }
 
