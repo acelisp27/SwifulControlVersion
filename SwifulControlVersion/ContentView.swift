15b3c3ff00f82bd1e3de02adee77e5af87f06157
AndresCelis <celisp27@outlook.com>
2025-07-18 15:36:59 -0500

    [Feature] Adding new rectangle

--- SwifulControlVersion/ContentView.swift
+++ SwifulControlVersion/ContentView.swift
@@ -57,7 +57,7 @@ struct ContentView: View {
             Button("Subscribe now"){
                 
             }
-            
+             Rectangle()
             
         }
         .padding()
