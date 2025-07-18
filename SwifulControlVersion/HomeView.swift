d314e480d8ef9318ad0664ba9b8be87758b16a91
AndresCelis <celisp27@outlook.com>
2025-07-18 11:34:37 -0500

    Add HomeView

---
+++ SwifulControlVersion/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwifulControlVersion
+//
+//  Created by Andres Celis on 18/07/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
