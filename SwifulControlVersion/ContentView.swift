0b12793f4c16f148908d610c15974a554f8b57d7
AndresCelis <celisp27@outlook.com>
2025-07-18 10:00:38 -0500

    Initial Commit

---
+++ SwifulControlVersion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwifulControlVersion
+//
+//  Created by Andres Celis on 18/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwifulControlVersion/SwifulControlVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifulControlVersionApp.swift
+//  SwifulControlVersion
+//
+//  Created by Andres Celis on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwifulControlVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
